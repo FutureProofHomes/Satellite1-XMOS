a4d9c18552a712f3f4d8086c80ca1d111f7f424e
Mischa Siekmann <mischa@gnumpi.com>
2025-03-12 13:19:15 +0100

    added orbit library, build usb_ns target only

---
+++ orbit/orbit/__init__.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+PROJ_ROOT = Path(__file__).parent.parent
+
+XTC_PATH="/Users/mischa/Projects/FutureProofHomes/XMOS_XTC_15.3.1"
+
+BOARD_USB_IDS = {
+    "Satellite1" : "20b1:4a05",
+    "Explorer" : "20b1:4001"
+}
+
+BOARD_SND_CARD_NAMES = {
+    "Satellite1" : "SATELLITE1",
+    "Explorer" : "XCORE-VOICE"
+}
+
+BOARD_XTAG_IDS = {
+    "Satellite1" : "W256H8UD",
+    "Explorer" : "4LG2K2A6"
+}
