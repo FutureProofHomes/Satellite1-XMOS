946c2ce8fe26355fcc97ced3df1a68d82bacd9c6
Mischa Siekmann <mischa@gnumpi.com>
2026-02-19 09:04:19 +0100

    add XK-VOICE-SQ66 bsp_config

---
+++ satellite-xmos-firmware/bsp_config/XK-VOICE-SQ66/platform/platform_start.c
@@ -0,0 +1,95 @@
+// Copyright 2022-2024 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
+/* System headers */
+#include <platform.h>
+
+/* FreeRTOS headers */
+#include "FreeRTOS.h"
+
+/* Library headers */
+#include "fs_support.h"
+
+/* App headers */
+#include "platform_conf.h"
+#include "platform/driver_instances.h"
+
+#if appconfDEVICE_CTRL_SPI
+#include "device_control_spi.h"
+#endif
+
+
+static void gpio_start(void)
+{
+    rtos_gpio_rpc_config(gpio_ctx_t0, appconfGPIO_T0_RPC_PORT, appconfGPIO_RPC_PRIORITY);
+    rtos_gpio_rpc_config(gpio_ctx_t1, appconfGPIO_T1_RPC_PORT, appconfGPIO_RPC_PRIORITY);
+
+#if ON_TILE(0)
+    rtos_gpio_start(gpio_ctx_t0);
+#endif
+#if ON_TILE(1)
+    rtos_gpio_start(gpio_ctx_t1);
+#endif
+}
+
+static void flash_start(void)
+{
+#if ON_TILE(FLASH_TILE_NO)
+    uint32_t flash_core_map = ~((1 << appconfUSB_INTERRUPT_CORE) | (1 << appconfUSB_SOF_INTERRUPT_CORE));
+    rtos_qspi_flash_start(qspi_flash_ctx, appconfQSPI_FLASH_TASK_PRIORITY);
+    rtos_qspi_flash_op_core_affinity_set(qspi_flash_ctx, flash_core_map);
+#endif
+}
+
+
+static void spi_start(void)
+{
+#if appconfDEVICE_CTRL_SPI && ON_TILE(SPI_CLIENT_TILE_NO)
+    rtos_spi_slave_start(spi_slave_ctx,
+                         device_control_spi_ctx,
+                         (rtos_spi_slave_start_cb_t) device_control_spi_start_cb,
+                         (rtos_spi_slave_xfer_done_cb_t) device_control_spi_xfer_done_cb,
+                         appconfSPI_INTERRUPT_CORE,
+                         appconfSPI_TASK_PRIORITY);
+#endif
+}
+
+static void mics_start(void)
+{
+    rtos_mic_array_rpc_config(mic_array_ctx, appconfMIC_ARRAY_RPC_PORT, appconfMIC_ARRAY_RPC_PRIORITY);
+
+#if ON_TILE(MICARRAY_TILE_NO)
+    rtos_mic_array_start(
+            mic_array_ctx,
+            2 * MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME,
+            appconfPDM_MIC_INTERRUPT_CORE);
+#endif
+}
+
+static void i2s_start(void)
+{
+#if appconfI2S_ENABLED
+    rtos_i2s_rpc_config(i2s_ctx, appconfI2S_RPC_PORT, appconfI2S_RPC_PRIORITY);
+
+#if ON_TILE(I2S_TILE_NO)
+    rtos_i2s_start(
+            i2s_ctx,
+            rtos_i2s_mclk_bclk_ratio(appconfAUDIO_CLOCK_FREQUENCY, appconfI2S_AUDIO_SAMPLE_RATE),
+            I2S_MODE_I2S,
+            2.2 * MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME * 3,
+            1.2 * MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME * 3,
+            appconfI2S_INTERRUPT_CORE);
+#endif
+#endif
+}
+
+
+void platform_start(void)
+{
+    rtos_intertile_start(intertile_ctx);
+    gpio_start();
+    flash_start();
+    spi_start();
+    mics_start();
+    i2s_start();
+}
