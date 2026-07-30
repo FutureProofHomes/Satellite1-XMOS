f1e2f902bb93d6c716938316a7fa929ed3398238
Mischa Siekmann <mischa@gnumpi.com>
2026-07-30 07:04:47 +0200

    wire device-control servicer count

--- satellite-xmos-firmware/bsp_config/SATELLITE1/platform/platform_init.c
+++ satellite-xmos-firmware/bsp_config/SATELLITE1/platform/platform_init.c
@@ -9,6 +9,7 @@
 #include "platform/app_pll_ctrl.h"
 #include "platform/driver_instances.h"
 #include "platform/platform_init.h"
+#include "control/device_control_servicer_config.h"
 
 static void mclk_init(chanend_t other_tile_c)
 {
@@ -99,7 +100,7 @@ static void spi_init(void)
     
     device_control_init(device_control_spi_ctx,
                         DEVICE_CONTROL_HOST_MODE,
-                        3, //number of servicers
+                        APP_DEVICE_CTRL_TOTAL_SERVICER_COUNT,
                         client_intertile_ctx,
                         1); 
     
---
+++ satellite-xmos-firmware/src/control/device_control_servicer_config.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifndef appconfAUDIO_CFG_SERVICER_COMPAT_ENABLED
+#define appconfAUDIO_CFG_SERVICER_COMPAT_ENABLED 1
+#endif
+
+#ifndef appconfLED_RING
+#define appconfLED_RING 0
+#endif
+
+#ifndef BUILTIN_TESTS_SPI_ECHO_SERVICER
+#define BUILTIN_TESTS_SPI_ECHO_SERVICER 0
+#endif
+
+/*
+ * Base SQ66/SATELLITE1 servicers under appconfDEVICE_CTRL_SPI:
+ * - GPIO servicer
+ * - DFU servicer
+ * - Audio pipeline tile0 servicer (mic output)
+ * - Audio pipeline tile1 servicer (speaker + mic input)
+ */
+#define APP_DEVICE_CTRL_BASE_SERVICER_COUNT 4
+
+#define APP_DEVICE_CTRL_TOTAL_SERVICER_COUNT \
+    (APP_DEVICE_CTRL_BASE_SERVICER_COUNT + \
+     appconfAUDIO_CFG_SERVICER_COMPAT_ENABLED + \
+     appconfLED_RING + \
+     !!(BUILTIN_TESTS_SPI_ECHO_SERVICER))
