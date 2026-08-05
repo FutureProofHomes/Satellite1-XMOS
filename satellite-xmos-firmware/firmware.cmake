53ed54972792cf38d479b7fe68c932a62d9f4e6a
Mischa Siekmann <mischa@gnumpi.com>
2026-08-05 10:19:08 +0200

    retire USB firmware variants

--- satellite-xmos-firmware/firmware.cmake
+++ satellite-xmos-firmware/firmware.cmake
@@ -90,5 +90,3 @@ endif()
 # XMOS Firmware Targets
 #**********************
 include(${CMAKE_CURRENT_LIST_DIR}/satellite1.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/satellite1-usb.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/explorer_devboard.cmake)
